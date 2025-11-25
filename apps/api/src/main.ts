8c8d48cbb1a29b44f5d4246a289b9ed30eb07644
ThanhTung19431261 <220455853+Wltung@users.noreply.github.com>
2025-11-25 09:40:03 +0700

    Fix: Import cookie-parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
